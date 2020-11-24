ecbfb80b4cd8dc902dcecedfad4583b6b6b61ab2
Mxctr1 <elmoctar.ih@gmail.com>
2020-11-24 13:18:07 -0500

    Ajout Javascript / Nouvelle partie/fin-partie etc

--- js/script.js
+++ js/script.js
@@ -3,27 +3,58 @@ document.addEventListener("DOMContentLoaded", function (event) {
     //Objet pour le jeu
     class Jeu {
 
-        constructor() {
+        constructor(_idSvg, _idPointage) {
+
+            this.s = Snap(_idSvg);
+            this.sortiePointage = document.querySelector(_idPointage);
+            this.grandeurCarre = 20;
+            this.grandeurGrille = 15;
+
+        }
+
+        nouvellePartie() {
+            this.affichagePointage(1);
+            this.pomme = new Pomme();
+            this.serpent = new Serpent();
+        }
+
+        finPartie() {
+
+
+        }
+
+        affichagePointage(_lePointage) {
+
+            this.sortiePointage.innerHTML = _lePointage;
 
         }
     }
 
 
-    //Objet Le serpent
+//Objet Le serpent
     class Serpent {
         constructor() {
+            console.log("serpent");
         }
     }
 
 
-    //Objet La pomme
+//Objet La pomme
     class Pomme {
         constructor() {
+            console.log("pomme");
         }
     }
 
 
-    var unePartie = new Jeu();
+    let unePartie = new Jeu("#jeu", "#pointage");
+
+    let btnJouer = document.querySelector("#btnJouer");
+    btnJouer.addEventListener("click", nouvellePartie);
+
+    function nouvellePartie() {
+        unePartie.nouvellePartie();
+    }
 
 
 });
